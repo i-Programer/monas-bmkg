d9fd3f19959956978f1b059d99146e82222ab436
i-Programer <ashof.z17@gmail.com>
2024-09-16 20:48:32 +0700

    fix legend and layer map positioning

--- resources/js/Components/MainComponent/Utility/LayerMap.jsx
+++ resources/js/Components/MainComponent/Utility/LayerMap.jsx
@@ -4,28 +4,28 @@ import { WiHumidity, WiThermometer, WiRain } from "react-icons/wi";
 const LayerMap = ({ activeTab, setActiveTab, selectedStation }) => {
     return (
         <>
-            <div className={`absolute left-5 top-[12rem] z-[500]`}>
-                <div className="flex flex-col justify-between items-start gap-y-4 text-white text-sm font-bold">
+            <div className={`absolute left-5 top-[3rem] z-[500]`}>
+                <div className="flex flex-col justify-between items-start gap-y-4 text-white text-sm font-bold w-[7rem]">
                     <button
-                        className="rounded-md  bg-green-700 p-1 flex flex-row justify-between items-center"
+                        className="rounded-md  bg-green-700 p-1 flex flex-row justify-stretch items-center w-full text-center"
                         onClick={() => setActiveTab(1)}
                     >
-                        <WiRain className="text-lg font-bold"/>
-                        <span>Precipitation</span>
+                        <WiRain className="text-2xl font-bold"/>
+                        <span className="text-cente">Precipitation</span>
                     </button>
                     <button
-                        className="rounded-md  bg-blue-700 p-1 flex flex-row justify-between items-center"
+                        className="rounded-md  bg-blue-700 p-1 flex flex-row justify-stretch items-center w-full text-center"
                         onClick={() => setActiveTab(2)}
                     >
-                        <WiHumidity className="text-lg font-bold"/>
-                        <span>Humidity</span>
+                        <WiHumidity className="text-2xl font-bold"/>
+                        <span className="text-cente">Humidity</span>
                     </button>
                     <button
-                        className="rounded-md  bg-red-700 p-1 flex flex-row justify-between items-center"
+                        className="rounded-md  bg-red-700 p-1 flex flex-row justify-stretch items-center w-full text-center"
                         onClick={() => setActiveTab(3)}
                     >
-                        <WiThermometer className="text-lg font-bold"/>
-                        <span>Temperature</span>
+                        <WiThermometer className="text-2xl font-bold"/>
+                        <span className="text-cente">Temperature</span>
                     </button>
                 </div>
             </div>
--- resources/js/Components/MainComponent/Utility/Legend.jsx
+++ resources/js/Components/MainComponent/Utility/Legend.jsx
@@ -23,7 +23,7 @@ const Legend = ({ min, max, mean, activeTab }) => {
     const meanPosition = `${((mean - min) / (max - min)) * 100}%`;
 
     return (
-        <div className="flex flex-col items-center absolute z-[590] mt-[23%] ml-4">
+        <div className="flex flex-col items-center absolute z-[590] mt-[17rem] ml-4">
             <div className="mb-2 text-white font-bold text-base">
                 Max: {max}
             </div>
