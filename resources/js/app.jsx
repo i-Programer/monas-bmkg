781e3615ecf8d0e8c8ae25d276765e62c9e3f3a7
i-Programer <ashof.z17@gmail.com>
2024-09-21 10:31:19 +0700

    change the website title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,7 +9,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = 'Monas';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}  ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
